d796dca28b3ebcbf054a5d2deffc1e350a2f8866
Mitochondria99 <Nayankubade99@gmail.com>
2023-09-25 23:31:04 +0530

    Recheck

--- test/LiquidityVault.t.sol
+++ test/LiquidityVault.t.sol
@@ -1,3 +1,6 @@
+//check authorization test video
+// consider priceFeed
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
@@ -16,58 +19,5 @@ contract LiquidityVaultTest is Test {
         vault = new LiquidityVault(mockToken, "VaultToken", "VT");
         alice = makeAddr("alice");
         bob = makeAddr("bob");
-    }
-
-    function test_Deposit() public {
-        uint256 depositAmount = 100 ether;
-        mockToken.mint(address(this), depositAmount);
-        mockToken.transfer(alice, depositAmount);
-
-        vm.prank(alice);
-        mockToken.transferFrom(address(this), address(vault), depositAmount);
-        vault.deposit(depositAmount);
-        //  assertEq(vault.balanceOf(alice) > 0, "Alice receive vault tokens");
-    }
-
-    function test_Withdraw() public {
-        uint256 depositAmount = 100 ether;
-        uint256 withdrawShares = 50 ether; // Assuming 1:1 conversion for simplicity
-
-        mockToken.mint(address(this), depositAmount);
-        mockToken.transfer(bob, depositAmount);
-
-        vm.prank(bob);
-        mockToken.transferFrom(address(this), address(vault), depositAmount);
-        vault.deposit(depositAmount);
-        uint256 bobStartingBalance = mockToken.balanceOf(bob);
-        vault.withdraw(withdrawShares);
-        assertEq(
-            mockToken.balanceOf(bob) > bobStartingBalance,
-            "Bob should have more MockToken after withdrawal"
-        );
-    }
-
-    function test_Release() public {
-        uint256 reserveAmount = 50 ether;
-        vault.authorizedAddresses(address(this)) = true;
-        vault.reserve(reserveAmount);
-
-        uint256 initialReservedLiquidity = vault.reservedLiquidity();
-        vault.release(reserveAmount);
-        assertEq(
-            vault.reservedLiquidity() < initialReservedLiquidity,
-            "Reserved liquidity should decrease after release"
-        );
-    }
-
-    function test_Reserve() public {
-        uint256 reserveAmount = 50 ether;
-        vault.authorizedAddresses(address(this)) = true;
-        uint256 initialReservedLiquidity = vault.reservedLiquidity();
-        vault.reserve(reserveAmount);
-        assertEq(
-            vault.reservedLiquidity() > initialReservedLiquidity,
-            "Reserved liquidity should increase after reserve"
-        );
     }
 }
